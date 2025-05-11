9d01d191ff8ff2e18255ea1d6f51a4bd77d74897
Milena Lingua <lingua.milena@gmail.com>
2025-05-11 16:34:21 -0300

    Ejercicios resuelto hasta el 15. Agrego style.css y carpeta img para la resolución.

--- Ejercicios.js
+++ Ejercicios.js
@@ -145,12 +145,172 @@ function agregarItem()
     boton.textContent = 'Eliminar item ' + numeroActual;    
     lista.appendChild(boton);
     boton.addEventListener("click", function() {
-        eliminarItem(numeroActual);        
+        lista.removeChild(item);
+        boton.remove();
+        //eliminarItem(numeroActual);        
     });
     numero++;
 }
-function eliminarItem(numeroItem)
+// function eliminarItem(numeroItem)
+// {
+//     document.getElementById('item' + numeroItem).remove();
+//     document.getElementById('eliminarItem' + numeroItem).remove();    
+// }
+
+/*Front End */
+/*Botón que muestra un mensaje
+Crear una página con un botón. Al hacer click, debe aparecer un mensaje debajo (como un saludo o bienvenida). El mensaje debe estar oculto inicialmente.*/
+
+let botonMensaje = document.getElementById('botonMensaje');
+let mensaje5 = document.getElementById('mensaje5');
+botonMensaje.addEventListener('click', () => 
 {
-    document.getElementById('item' + numeroItem).remove();
-    document.getElementById('eliminarItem' + numeroItem).remove();    
-}
+    mensaje5.textContent = "Bienvenid@";
+});
+
+/*Cambiar color de fondo
+Hacer una página con un botón que, al hacer click, cambie el color de fondo del body. Debe rotar entre varios colores (mínimo 3). */
+
+let colorBody = document.getElementById('colorBody');
+let body = document.querySelector('body');
+colorBody.addEventListener('click', () => 
+{
+    switch (body.style.backgroundColor) {
+        case '':
+            body.style.backgroundColor = 'yellow';
+            break;
+        case 'yellow':
+            body.style.backgroundColor = 'red'
+            break;
+        case 'red':
+            body.style.backgroundColor = 'orange'
+            break;
+        case 'orange':
+            body.style.backgroundColor = ''
+            break;
+        default:
+            break;
+    }
+});
+
+/*Mostrar/Ocultar texto
+Incluir un párrafo visible y un botón que permita ocultarlo o mostrarlo según su estado actual. El texto del botón debe cambiar entre “Mostrar” y “Ocultar”. */
+
+let textoOcultar = document.getElementById('textoOcultar');
+let ocultarMostrar = document.getElementById('ocultarMostrar');
+textoOcultar.style.visibility = 'visible';
+ocultarMostrar.addEventListener('click', () => 
+{
+    switch (textoOcultar.style.visibility) {
+        case 'visible':
+            textoOcultar.style.visibility = 'hidden';
+            ocultarMostrar.textContent = 'Mostrar'
+            break;
+        case 'hidden':
+            textoOcultar.style.visibility = 'visible';
+            ocultarMostrar.textContent = 'Ocultar'
+            break;     
+        default:
+            break;
+    }
+});
+
+/*Actualizar título en tiempo real
+Crear un campo de texto y un título (<h2>). A medida que el usuario escribe, el texto del título debe actualizarse en tiempo real. */
+
+let inputTitulo = document.getElementById('inputTitulo');
+let tituloActualizar = document.getElementById('tituloActualizar');
+
+inputTitulo.addEventListener('input', () => 
+{
+    let titulo = inputTitulo.value;
+    tituloActualizar.textContent = titulo;
+});
+
+/*Aplicar estilos a varios elementos
+Agregar varios párrafos con una clase compartida. Al presionar un botón, debe cambiar el fondo de esos elementos (resaltarlos visualmente). */
+
+let resaltados = document.getElementsByClassName('resaltado');
+let cambiarResaltado = document.getElementById('cambiarResaltado');
+cambiarResaltado.addEventListener('click', () =>
+{
+    Array.from(resaltados).forEach((x) => {
+        x.style.backgroundColor = 'yellow';
+    });
+});
+
+/*Validación de formulario simple
+Crear un pequeño formulario con nombre y correo. Al hacer click en enviar, debe validar que ambos campos estén completos. Muestra un mensaje de error o éxito según el caso. */
+
+let botonEnviar = document.getElementById('enviar');
+let nombre = document.getElementsByName('nombre');
+let email = document.getElementsByName('email');
+
+/*Generador de lista de tareas
+Crear una lista de tareas con un input y botón. Cada tarea debe incluir un botón para marcarla como completada (tachada). */
+
+let inputTarea = document.getElementById('tarea');
+let agregarTarea = document.getElementById('agregarTarea');
+let lista = document.getElementById('listaTareas');
+
+agregarTarea.addEventListener('click', () => 
+{
+    let item = document.createElement('li');
+    item.textContent = inputTarea.value;
+    lista.appendChild(item);
+    let check = document.createElement('input');
+    check.type = 'checkbox';
+    item.append(check);
+    inputTarea.value = '';
+    check.addEventListener('change', () => 
+    {
+        switch(check.checked)
+        {
+            case true: 
+                item.style.textDecoration = 'line-through';
+                break;
+            case false:
+                item.style.textDecoration = 'none';
+        }
+    })
+});
+
+/*Mostrar longitud de texto ingresado
+Incluir un input de texto y, mientras el usuario escribe, muestra la cantidad de caracteres ingresados. */
+
+let texto = document.getElementById('textoLongitud');
+let mostrarLongitud = document.getElementById('longitud');
+
+texto.addEventListener('input', () =>
+{
+    let largo = texto.value.length;
+    mostrarLongitud.textContent = largo;
+});
+
+/*Cambiar imagen con botón
+Mostrar una imagen en pantalla. Al hacer click en un botón, debe cambiar por otra imagen (usa 2 o más imágenes disponibles). */
+
+let imagen = document.getElementById('imagen');
+let cambiarImagen = document.getElementById('cambiarImagen');
+let estadoActual = 'html';
+cambiarImagen.addEventListener('click', () => 
+{    
+    
+    switch(estadoActual)
+    {
+        case 'html':
+            imagen.src = './img/css_640.png';
+            estadoActual = 'css';
+            break;
+        case 'css':
+            imagen.src = './img/js_640.png';
+            estadoActual = 'js';
+            break;
+        case 'js':
+            imagen.src = './img/html_640.png';
+            estadoActual = 'html';
+            break;
+        default:
+            break;
+    }
+});
